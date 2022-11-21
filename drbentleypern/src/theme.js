fa3a778cc1cdeb5733779e2d866d82a2690cb8c4
jayhawkJ.Alexander <jordanh316@gmail.com>
2022-11-21 16:58:53 +0000

    Update theme.js

--- drbentleypern/src/theme.js
+++ drbentleypern/src/theme.js
@@ -1,38 +1,30 @@
 import createTheme from "@mui/material/styles/createTheme";
 
-export  const theme = createTheme({
-    palette: {
-        type: 'dark',
-        primary: {
-            main: '#1AB4B1',
-            dark: '#880029',
-            contrastText: 'rgba(103,209,214,0.98)',
-        },
-        secondary: {
-            main: '#CF38BC',
-            light: '#48a999',
-            dark: '#004c40',
-            contrastText: '#9c2b41',
-        },
-        text: {
-            hint: '#ffebee',
-            primary:'#da820c',
-        },
-        background: {
-            default: '#0c0b0b',
-            paper: '#433f3f',
-        },
-        divider: '#10d289',
-    },
-    typography: {
-        fontFamily: 'Lora',
-        h5: {
-            fontFamily: 'Roboto',
-        },
-    },
-    shape: {
-        borderRadius: 4,
-    },
-    spacing: 8,
 
-})
+export const theme = createTheme = ({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#0a2119',
+      light: '#13f9f9',
+    },
+    secondary: {
+      main: '#3b8da5',
+    },
+    background: {
+      default: '#171200',
+    },
+    text: {
+      primary: '#11e6a7',
+    },
+  },
+  typography :{
+     fontFamily: "Roboto"
+  },
+  shape: {
+  borderRadius: 4,
+  },
+  spacing: 8,
+});
+
+
